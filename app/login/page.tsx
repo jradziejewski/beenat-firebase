b15f6bf595eec537e8a6467358a1043330392701
jradziejewski <261309@student.pwr.edu.pl>
2023-08-17 23:30:40 +0200

    feat: add very basic landing page template

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from "next";
-import Image from "next/image";
 import Link from "next/link";
-
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
 import { UserAuthForm } from "@/components/Auth/Auth";
 
 export const metadata: Metadata = {
@@ -13,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <div className="h-full flex align-middle">
+    <div className="h-full flex">
       <div className="mx-auto flex w-full flex-col justify-center my-auto space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <main>Some content</main>
+    <div className="h-full flex flex-col lg:flex-row">
+      <main className="m-auto">
+        <h1 className="text-9xl">Your live events</h1>
+        <p className="text-4xl mt-10">right here</p>
+      </main>
+      <section className="m-auto">
+        <h1>Sign up</h1>
+      </section>
     </div>
   );
 }
