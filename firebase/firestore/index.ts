4dfcb69189e160758be125645c94f74721c52e5e
jradziejewski <261309@student.pwr.edu.pl>
2023-09-10 12:23:43 +0200

    refactor: keep all firestore functions in same file

--- firebase/firestore/index.ts
+++ firebase/firestore/index.ts
@@ -1,5 +1,54 @@
-import addData from "./addData";
-import getData from "./getData";
-import deleteDocument from "./deleteDocument";
+import { EventItem } from "@/types";
+import { db } from "../config";
+import {
+  addDoc,
+  collection,
+  doc,
+  deleteDoc,
+  getDocs,
+} from "firebase/firestore";
 
-export { addData, getData, deleteDocument };
+export async function addData(collectionName: string, data: EventItem) {
+  let result = null;
+  let error = null;
+
+  try {
+    result = await addDoc(collection(db, collectionName), data);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
+
+export async function getDocument(collectionName: string) {
+  let result = null;
+  let error = null;
+
+  try {
+    result = await getDocs(collection(db, collectionName));
+  } catch (e) {
+    error = e;
+  }
+
+  const resultArr: Array<EventItem> = [];
+
+  result?.forEach((doc) => {
+    resultArr.push({ id: doc.id, ...doc.data() } as EventItem);
+  });
+
+  return { resultArr, error };
+}
+
+export async function deleteDataEntry(collectionName: string, id: string) {
+  let result = null;
+  let error = null;
+
+  try {
+    result = await deleteDoc(doc(db, collectionName, id));
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
